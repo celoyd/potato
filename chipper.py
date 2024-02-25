51dcdf4de13f11a9bfde6b97d4563bab18c206a4
celoyd <celoyd@gmail.com>
2024-02-24 19:02:48 -0800

    Reorg/light rewrite of chip handling

--- chipper.py
+++ chipper.py
@@ -1,15 +1,27 @@
 import datetime
 import json
+import pathlib
 from pathlib import Path
-from sys import stdout
-import numpy as np
+
+import sys
+
+# from sys import stdout, stdout
+# import numpy as np
+
 import rasterio as rio
 import torch
 from einops import rearrange
 from torch import nn
-from typing import Sequence, Tuple
+from torch.nn import functional as F
+
+# from typing import Sequence, Tuple
 from torch.utils.data import DataLoader, Dataset
-from sys import argv
+
+import click
+import logging
+from pyproj import Transformer
+
+phi = (1 + 5**0.5) / 2
 
 
 # PixelShuffle operations by other names:
@@ -23,6 +35,80 @@ def tile(x, factor):
     return rearrange(
         x, "... (c f0 f1) h w -> ... c (h f0) (w f1)", f0=factor, f1=factor
     )
+
+
+def cheap_half(x):
+    # Fast 2× downsample
+    return (
+        x[..., 0::2, 0::2]
+        + x[..., 0::2, 1::2]
+        + x[..., 1::2, 0::2]
+        + x[..., 1::2, 1::2]
+    ) / 4.0
+
+
+# Todo: factor this color stuff into its own mini-library
+
+mul_to_xyz_matrix = torch.tensor(
+    [
+        [9.0677e-02, 0.0000e00, 4.0220e-01],
+        [1.1873e-01, 1.5157e-01, 7.5022e-01],
+        [2.0263e-01, 5.8714e-01, 0.0000e00],
+        [5.0440e-01, 2.9138e-01, 1.7287e-04],
+        [8.7978e-02, 2.7045e-02, 7.2419e-07],
+        [0.0000e00, 3.2608e-04, 0.0000e00],
+        [1.0887e-04, 1.7025e-04, 0.0000e00],
+        [8.0838e-05, 5.0579e-05, 0.0000e00],
+    ]
+)
+
+
+xyz_to_oklab_m1 = torch.tensor(
+    [
+        [0.8189330101, 0.3618667424, -0.1288597137],
+        [0.0329845436, 0.9293118715, 0.0361456387],
+        [0.0482003018, 0.2643662691, 0.6338517070],
+    ]
+)
+
+xyz_to_oklab_m2 = torch.tensor(
+    [
+        [0.2104542553, 0.7936177850, -0.0040720468],
+        [1.9779984951, -2.4285922050, 0.4505937099],
+        [0.0259040371, 0.7827717662, -0.8086757660],
+    ]
+)
+
+
+class OldSatellite(Exception):
+    pass
+
+
+class TooManyNulls(Exception):
+    pass
+
+
+def safe_pow(n, exp):
+    return n.sign() * n.abs().pow(exp)
+
+
+def xyz_to_lab(xyz):
+    lms = xyz_to_oklab_m1 @ xyz
+    lamasa = safe_pow(lms, 1 / 3)
+    Lab = xyz_to_oklab_m2 @ lamasa
+    return Lab
+
+
+def mul_to_xyz(mul):
+    return mul @ mul_to_xyz_matrix
+
+
+def mul_to_lab(mul):
+    xyz = torch.einsum("rhw, rx -> xhw", mul, mul_to_xyz_matrix)
+    lms = torch.einsum("xhw, mx -> mhw", xyz, xyz_to_oklab_m1)
+    lms = safe_pow(lms, 1 / 3)
+    oklab = torch.einsum("mhw, lm -> lhw", lms, xyz_to_oklab_m2)
+    return oklab
 
 
 class R2:
@@ -45,21 +131,17 @@ class R2:
         return (self.a1 * idx) % 1, (self.a2 * idx) % 1
 
 
-scattering = R2()
+class Tile(Dataset):
+    """
+    This represents one of the large (~5 km on a side) tiles in
+    Maxar’s ARD format. It’s intended to be used within a Chipper
+    object. For specifics of ARD, see the oficial documentation:
+    https://ard.maxar.com/docs/about/
+    """
 
-
-class OldSatellite(Exception):
-    pass
-
-
-class TooManyNulls(Exception):
-    pass
-
-
-class Chip(Dataset):
     def __init__(self, basepath: Path, cid: str):
         self.root_dir = basepath
-        self.cid = cid
+        self.cid = cid  # catalog ID
 
         self.json = json.load(open(basepath / f"{cid}.json"))
 
@@ -73,19 +155,24 @@ class Chip(Dataset):
         ):
             raise OldSatellite
 
-        # self.gsd_max = self.metadata["gsd_max"]
         self.pan = rio.open(self.root_dir / f"{self.cid}-pan.tif")
         self.mul = rio.open(self.root_dir / f"{self.cid}-ms.tif")
 
         self.mul_side = self.mul.profile["width"]
 
-    def validish(self, tensor):
-        minimum = 0.75
-        pixel_count = torch.prod(torch.tensor(tensor.shape))
-        valid_count = torch.count_nonzero(tensor)
-        return valid_count > (pixel_count * minimum)
+        self.to_latlon = Transformer.from_crs(self.mul.crs, 4326)
+        # print(self.to_latlon)
 
-    def read(self, side: int, proportion: Tuple[float, float]):
+    def coords(self, mul_window):
+        center = (
+            mul_window.col_off + mul_window.width / 2,
+            mul_window.row_off + mul_window.height / 2,
+        )
+        xy = rio.transform.xy(self.mul.transform, cols=center[0], rows=center[1])
+        lalo = self.to_latlon.transform(*xy)  # center[1], center[0])
+        return {"lat": round(lalo[0], 5), "lon": round(lalo[1], 5)}
+
+    def read(self, side, proportion):
         max_valid = self.mul_side - side
 
         left, top = (int(z * max_valid) % max_valid for z in proportion)
@@ -94,32 +181,39 @@ class Chip(Dataset):
         mul_window = rio.windows.Window(*mul_dims)
         pan_window = rio.windows.Window(*(4 * z for z in mul_dims))
 
-        mul = torch.tensor(self.mul.read(window=mul_window).astype(np.int16))
-        pan = torch.tensor(self.pan.read(window=pan_window).astype(np.int16))
-
-        if not ((self.validish(pan) and self.validish(mul))):
-            raise TooManyNulls
+        mul = torch.tensor(self.mul.read(window=mul_window).astype("float32"))
+        pan = torch.tensor(self.pan.read(window=pan_window).astype("float32"))
 
         pan = pile(pan, factor=4)
 
         pack = torch.cat((pan, mul), 0)
+        path = self.mul.name
+        coords = self.coords(mul_window)
 
-        return pack
+        return pack, path, coords
 
 
 class Chipper(Dataset):
-    def __init__(self, ard_roots: Sequence[Path], length: int):
-        self.root = ard_roots
+    """
+    Represents a set of ARDs and makes it easy to read dispersed
+    chips out of them. Covers operations like image degradation that
+    are not specific to a file (those goes in the Tile object).
+    """
+
+    def __init__(self, ard_roots, length, starting_from):
+
         self.length = length
+        self.starting_from = starting_from
 
         self.chips = []
 
         self.scattering = R2()
 
+        self.id_kernel = torch.tensor([[[[0, 0, 0], [0, 1, 0], [0, 0, 0]]]]).float()
+
         for ard_root in ard_roots:
             for metadata in ard_root.glob("acquisition_collections/*.json"):
                 with open(metadata) as md:
-                    # print(md)
                     md = json.load(md)
 
                 inner_json_path = [x["href"] for x in md["links"] if x["rel"] == "item"]
@@ -129,22 +223,209 @@ class Chipper(Dataset):
                 chip_cid = Path(inner_json_path).stem
 
                 try:
-                    chip = Chip(chip_dir, chip_cid)
+                    chip = Tile(chip_dir, chip_cid)
                     self.chips.append(chip)
-                except:
-                    pass
+                except OldSatellite:
+                    logging.warning(
+                        f"Skipping CID {chip_cid}: satellite is not WV-2 or -3"
+                    )
 
-    def __len__(self) -> int:
+    def validish(self, tensor):
+        minimum = 0.75
+        pixel_count = torch.prod(torch.tensor(tensor.shape))
+        valid_count = torch.count_nonzero(tensor)
+        return valid_count > (pixel_count * minimum)
+
+    def m_noise(self, shape, scale):
+        # Multiplicative noise centers on 1
+        return torch.normal(1.0, scale, shape)
+
+    def a_noise(self, shape, scale):
+        # Additive noise centers on 0
+        return torch.normal(0.0, scale, shape)
+
+    def cheap_half(self, x):
+        # Fast 2× downsample
+        return (
+            x[..., 0::2, 0::2]
+            + x[..., 0::2, 1::2]
+            + x[..., 1::2, 0::2]
+            + x[..., 1::2, 1::2]
+        ) / 4.0
+
+    def a_noisy_kernel(self, std=0.1):
+        noise = self.a_noise(self.id_kernel.shape, std)
+        noisy = self.id_kernel + noise
+        noisy /= noisy.sum()
+        noisy = noisy.expand(8, -1, -1, -1)
+        return noisy
+
+    def eight_noisy_kernels(self, std=0.1):
+        identities = self.id_kernel.expand(8, -1, -1, -1)
+        noise = self.a_noise(identities.shape, std)
+        noisy = identities + noise
+        sums = noisy.sum(dim=(-1, -2))
+        noisy /= sums.view(8, 1, 1, 1)
+        return noisy
+
+    def worsen(self, x, all_std=0.05, each_std=0.1):
+        all_k = self.a_noisy_kernel(all_std)
+        x = F.conv2d(x, all_k, groups=8, padding="same")
+
+        each_k = self.eight_noisy_kernels(each_std)
+        x = F.conv2d(x, each_k, groups=8, padding="same")
+
+        return x
+
+    def pack_to_xy(self, pack):
+        pack = pack.float() / 10_000
+
+        # pan = cheap_half(shuf2(y[:16]))
+        pan = cheap_half(tile(pack[:16], 4))
+        mul = cheap_half(pack[16:])
+
+        rots = int(torch.rand((1,)) * 4)
+        pan = torch.rot90(pan, dims=(-1, -2), k=rots)
+        mul = torch.rot90(mul, dims=(-1, -2), k=rots)
+
+        pan_down = cheap_half(cheap_half(pan)).unsqueeze(0)
+        # pan_down = unshuf2(pan_down)
+        pan_down = pile(pan_down, 4)
+
+        mul_down = cheap_half(cheap_half(mul)).unsqueeze(0)
+        mul_down = self.worsen(mul_down)
+
+        mul_down = mul_down * self.m_noise(
+            mul_down.shape, scale=1 / 250
+        ) + self.a_noise(mul_down.shape, scale=1 / 1_000)
+
+        pan_down = pan_down * self.m_noise(
+            pan_down.shape, scale=1 / 1_000
+        ) + self.a_noise(pan_down.shape, scale=1 / 2_000)
+
+        x = torch.squeeze(torch.cat([pan_down, mul_down], dim=1))
+
+        y = mul_to_lab(mul)
+
+        return x, y
+
+    def __len__(self):
         return self.length
 
-    def __getitem__(self, index: int) -> torch.Tensor:
+    def __getitem__(self, index):
         chip = index % len(self.chips)  # ?
 
-        x, y = self.scattering[index]
+        x_off, y_off = self.scattering[index + self.starting_from]
 
-        pack = self.chips[chip].read(256, (x, y))
-        return pack
+        pack, path, coords = self.chips[chip].read(1024, (x_off, y_off))
 
+        if not self.validish(pack):
+            raise TooManyNulls
+
+        x, y = self.pack_to_xy(pack)
+
+        return x, y, path, coords
+
+
+@click.group()
+@click.option(
+    "--log",
+    "-l",
+    default="/dev/stdout",  # this seems wrong
+    type=click.Path(exists=False, dir_okay=False, allow_dash=True, path_type=Path),
+)
+def cli(log):
+    logging.basicConfig(
+        filename=log,
+        format="%(asctime)s (%(levelname)s): %(message)s",
+        encoding="utf-8",
+        level=logging.INFO,
+    )
+
+
+@cli.command()
+@click.option(
+    "--ard-dir",
+    "-a",
+    required=True,
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+)
+@click.option(
+    "--chip-dir",
+    "-c",
+    required=True,
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+)
+@click.option(
+    "--count",
+    "-n",
+    required=True,
+    type=int,
+)
+@click.option("--starting-from", "-s", type=int, default=0)
+def make_chips(ard_dir, chip_dir, count, starting_from):
+    chipper = Chipper([ard_dir], count, starting_from)
+    completed = 0
+    failed = 0
+    n = starting_from
+
+    while completed < count:
+        try:
+            x, y, path, coords = chipper[n]
+            dst = chip_dir / f"{completed}.pt"
+            torch.save((x, y), dst)
+            logging.info(f"{dst} at {coords} from {path}")
+            completed += 1
+        except TooManyNulls:
+            failed += 1
+        n += 1
+
+    logging.info(f"Fishined {completed} chips. There were {failed} failures.")
+
+
+@cli.command()
+@click.argument(
+    "srcs", type=click.Path(exists=True, file_okay=False, path_type=Path), nargs=-1
+)
+@click.argument(
+    "dst", type=click.Path(exists=True, file_okay=False, path_type=Path), nargs=1
+)
+def link_chips(srcs, dst):
+    all_paths = []
+    for src in srcs:
+        all_paths += list(src.glob("*.pt"))
+
+    total = len(all_paths)
+    logging.info(f"Preparing to link {total} files from {tuple(str(s) for s in srcs)} to {dst}.")
+
+    for p in range(total):
+        if p > 0 and p % 1000 == 0:
+            logging.info(f"Completed {p} links.")
+
+        n = int((p * phi)) % len(
+            all_paths
+        )  # arguably random would be better but I feel fancy
+        pt = Path(all_paths[n])
+        link = Path(dst / f"{p}.pt")
+
+        try:
+            pathlib.os.symlink(pt, link)
+        except FileExistsError:
+            logging.critical(
+                f"{link} already exists. Will not delete or overwrite."
+            )
+            sys.exit()
+
+        all_paths = all_paths[:n] + all_paths[n + 1 :]
+
+    logging.info(f"Made {total} links in {dst}.")
+
+
+if __name__ == "__main__":
+    cli()
+
+"""
+scattering = R2()
 
 ard, chip_count, dst_dir = argv[1:]
 ard = Path(ard)
@@ -166,6 +447,9 @@ while done < chip_count:
         done += 1
     except TooManyNulls:
         failed += 1
+"""
+# if failed > done * 100:  # > (length / 2) and (tried / (done+1)) > 2:
+#    raise ValueError(f"Too many bad ones in {ard}.")
 
-    # if failed > done * 100:  # > (length / 2) and (tried / (done+1)) > 2:
-    #    raise ValueError(f"Too many bad ones in {ard}.")
+# python chipper.py make-chips ard chipdir --log chiplog1.log
+# python chipper.py link-chips chipdir/{foo,bar} chipdir/pool
--- train.py
+++ train.py
@@ -21,62 +21,7 @@ from tqdm import tqdm
 from model.ripple import Ripple
 from model.ripple import shuf2, unshuf2
 
-
-### The color part
-
-
-mul_to_xyz_matrix = torch.tensor(
-    [
-        [9.0677e-02, 0.0000e00, 4.0220e-01],
-        [1.1873e-01, 1.5157e-01, 7.5022e-01],
-        [2.0263e-01, 5.8714e-01, 0.0000e00],
-        [5.0440e-01, 2.9138e-01, 1.7287e-04],
-        [8.7978e-02, 2.7045e-02, 7.2419e-07],
-        [0.0000e00, 3.2608e-04, 0.0000e00],
-        [1.0887e-04, 1.7025e-04, 0.0000e00],
-        [8.0838e-05, 5.0579e-05, 0.0000e00],
-    ]
-)
-
-
-xyz_to_oklab_m1 = torch.tensor(
-    [
-        [0.8189330101, 0.3618667424, -0.1288597137],
-        [0.0329845436, 0.9293118715, 0.0361456387],
-        [0.0482003018, 0.2643662691, 0.6338517070],
-    ]
-)
-
-xyz_to_oklab_m2 = torch.tensor(
-    [
-        [0.2104542553, 0.7936177850, -0.0040720468],
-        [1.9779984951, -2.4285922050, 0.4505937099],
-        [0.0259040371, 0.7827717662, -0.8086757660],
-    ]
-)
-
-
-def safe_pow(n, exp):
-    return n.sign() * n.abs().pow(exp)
-
-
-def xyz_to_lab(xyz):
-    lms = xyz_to_oklab_m1 @ xyz
-    lamasa = safe_pow(lms, 1 / 3)
-    Lab = xyz_to_oklab_m2 @ lamasa
-    return Lab
-
-
-def mul_to_xyz(mul):
-    return mul @ mul_to_xyz_matrix
-
-
-def mul_to_lab(mul):
-    xyz = torch.einsum("rhw, rx -> xhw", mul, mul_to_xyz_matrix)
-    lms = torch.einsum("xhw, mx -> mhw", xyz, xyz_to_oklab_m1)
-    lms = safe_pow(lms, 1 / 3)
-    oklab = torch.einsum("mhw, lm -> lhw", lms, xyz_to_oklab_m2)
-    return oklab
+from pathlib import Path
 
 
 ### The loss part
@@ -104,104 +49,24 @@ def big_pyramid_loss(y, ŷ, wt="db8", chroma_weight=8, highres_weight=2):
     return sum / torch.prod(torch.tensor(y.shape))
 
 
-### The chip loading (and noising) part
-# This all happens on the CPU so multicore workers can do it
-# while the GPU focuses on GPU stuff. Todo: numpy->torch even here?
+### The chip loading part
 
-
-def m_noise(shape, scale):
-    # Multiplicative noise centers on 1
-    return torch.normal(1.0, scale, shape, device="cpu")
-
-
-def a_noise(shape, scale):
-    # Additive noise centers on 0
-    return torch.normal(0.0, scale, shape, device="cpu")
-
-
-def cheap_half(x):
-    # Fast 2× downsample
-    return (
-        x[..., 0::2, 0::2]
-        + x[..., 0::2, 1::2]
-        + x[..., 1::2, 0::2]
-        + x[..., 1::2, 1::2]
-    ) / 4.0
-
-
-class Chipper(Dataset):
-    def __init__(self, length: int, offset: int = 0):
+class ChipReader(Dataset):
+    def __init__(self, chip_dir, length, offset=0):
         self.length = length
         self.offset = offset
-
-        self.id_kernel = torch.tensor([[[[0, 0, 0], [0, 1, 0], [0, 0, 0]]]]).float()
-
-    def a_noisy_kernel(self, std=0.1):
-        noise = a_noise(self.id_kernel.shape, std)
-        noisy = self.id_kernel + noise
-        noisy /= noisy.sum()
-        noisy = noisy.expand(8, -1, -1, -1)
-        return noisy
-
-    def eight_noisy_kernels(self, std=0.1):
-        identities = self.id_kernel.expand(8, -1, -1, -1)
-        noise = a_noise(identities.shape, std)
-        noisy = identities + noise
-        sums = noisy.sum(dim=(-1, -2))
-        noisy /= sums.view(8, 1, 1, 1)
-        return noisy
-
-    def blur(self, x, all_std=0.1, each_std=0.1):
-        all_k = self.a_noisy_kernel(all_std)
-        x = F.conv2d(x, all_k, groups=8, padding="same")
-
-        each_k = self.eight_noisy_kernels(each_std)
-        x = F.conv2d(x, each_k, groups=8, padding="same")
-
-        return x
-
+        self.src = Path(chip_dir)
 
     def __len__(self) -> int:
         return self.length
 
-    def __getitem__(self, index: int):
-        y = torch.load(f"chips/pool/{index}.pt")
-
-        y = y.float() / 10_000
-
-        pan = cheap_half(shuf2(y[:16]))
-        mul = cheap_half(y[16:])
-
-        rots = int(torch.rand((1,)) * 4)
-
-        pan = torch.rot90(pan, dims=(-1, -2), k=rots)
-        mul = torch.rot90(mul, dims=(-1, -2), k=rots)
-
-
-        pan_down = cheap_half(cheap_half(pan)).unsqueeze(0)
-        pan_down = unshuf2(pan_down)
-
-        mul_down = cheap_half(cheap_half(mul)).unsqueeze(0)
-        mul_down = self.blur(mul_down)
-
-
-        mul_down = mul_down * m_noise(mul_down.shape, scale=1 / 500) + a_noise(
-            mul_down.shape, scale=1 / 1_000
-        )
-
-        pan_down = pan_down * m_noise(pan_down.shape, scale=1 / 1_000) + a_noise(
-            pan_down.shape, scale=1 / 2_500
-        )
-
-        x = torch.squeeze(torch.cat([pan_down, mul_down], dim=1))
-
-        y = mul_to_lab(mul)
-
+    def __getitem__(self, index):
+        x, y = torch.load(self.src / f"{index}.pt")
         return x, y
 
 
-physical_batch_size = 32
-logical_batch_size = 64
+physical_batch_size = 8
+logical_batch_size = 32
 
 loader_params = {
     "batch_size": physical_batch_size,
@@ -210,22 +75,22 @@ loader_params = {
     "pin_memory": True,
 }
 
-trainlen = 24 * 1024
+trainlen = 2 * 1024
 testlen = 64
 
-Train = Chipper(
+Train = ChipReader(
+    "chips2",
     trainlen,
     0,
 )
 
-Test = Chipper(testlen, trainlen)
+Test = ChipReader("chips2", testlen, trainlen)
 
 trainloader = DataLoader(Train, **loader_params)
 testloader = DataLoader(Test, **loader_params)
 
 
 ### The training part
-
 
 @click.command()
 @click.option("--session", default="space_heater", help="Name of training session")
@@ -321,8 +186,6 @@ def train(session, load_epoch, lr, epochs):
 
             te += 1
 
-
-# device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 
 log = SummaryWriter()
