297fde61e9f95574036629f2a6a72cf1a86b8386
celoyd <celoyd@gmail.com>
2024-02-27 21:55:51 -0800

    Turning things in to CLI params and fixing horrible mistakes

--- train.py
+++ train.py
@@ -35,12 +35,6 @@ l1_criterion = nn.L1Loss(reduction="mean")
 dtcwt = DTCWTForward(J=3, biort="near_sym_b", qshift="qshift_b").to(device)
 
 
-def saturation_loss(y, ŷ):
-    ysat = torch.sqrt(torch.square(y[:, 0]) + torch.square(y[:, 1]))
-    ŷsat = torch.sqrt(torch.square(ŷ[:, 0]) + torch.square(ŷ[:, 1]))
-    return torch.mean(torch.abs(ysat - ŷsat))
-
-
 def ΔEuOK(y, ŷ, a_weight=1.0, b_weight=1.0):
     """
     Ordinary 3D distance in the oklab space.
@@ -62,14 +56,17 @@ def ΔEOK(y, ŷ, c_weight=1.0, h_weight=1.0):
     # https://github.com/svgeesus/svgeesus.github.io/blob/master/Color/OKLab-notes.md#color-difference-metric
     # For oklab notes please see ΔEuOK.
     ΔL = y[:, 0] - ŷ[:, 0]
-    C1 = torch.sqrt(torch.square(y[:, 1]) + torch.square(y[:, 2]))
-    C2 = torch.sqrt(torch.square(ŷ[:, 1]) + torch.square(ŷ[:, 2]))
+    C1 = torch.sqrt(torch.square(y[:, 1]) + torch.square(ŷ[:, 1]) + 1e-6)
+    C2 = torch.sqrt(torch.square(y[:, 2]) + torch.square(ŷ[:, 2]) + 1e-6)
     ΔC = C1 - C2
     Δa = y[:, 1] - ŷ[:, 1]
     Δb = y[:, 2] - ŷ[:, 2]
-    ΔH = torch.sqrt(torch.square(Δa) + torch.square(Δb) + torch.square(ΔC))
+    ΔH = torch.sqrt(torch.square(Δa) + torch.square(Δb) + torch.square(ΔC) + 1e-6)
     return torch.sqrt(
-        torch.square(ΔL) + torch.square(ΔC * c_weight) + torch.square(ΔH * h_weight)
+        torch.square(ΔL)
+        + torch.square(ΔC * c_weight)
+        + torch.square(ΔH * h_weight)
+        + 1e-6
     ).mean()
 
 
@@ -83,9 +80,6 @@ def big_pyramid_loss(y, ŷ, wt="db8"):
         sum += torch.sum(torch.abs(yh[lev] - ŷh[lev]))
 
     return sum / torch.prod(torch.tensor(y.shape))
-
-
-### The chip loading part
 
 
 class ChipReader(Dataset):
@@ -102,47 +96,56 @@ class ChipReader(Dataset):
         return x, y
 
 
-physical_batch_size = 8
-logical_batch_size = 32
-
-loader_params = {
-    "batch_size": physical_batch_size,
-    "shuffle": True,
-    "num_workers": 0 if str(device) == 'mps' else 4,
-    "pin_memory": True,
-}
-
-# trainlen = 3 * 1024
-# testlen = 64
-trainlen = 10
-testlen = 10
-
-Train = ChipReader(
-    #"/media/ch/salp/lc4/pools/sudan/",
-    # "chips5",
-    "chips",
-    trainlen,
-    0,
-)
-
-# Test = ChipReader("/media/ch/salp/lc5/pools/sudan/", testlen, trainlen)
-Test = ChipReader("chips/", testlen, trainlen)
-
-trainloader = DataLoader(Train, **loader_params)
-testloader = DataLoader(Test, **loader_params)
-
-
 ### The training part
 
 
 @click.command()
 @click.option("--session", default="space_heater", help="Name of training session")
-@click.option("--load-epoch", default=0, help="Completed epoch to start from.")
-@click.option("--lr", default=5e-4, help="Learning rate.")
-@click.option("--epochs", default=320, help="Epochs to train for.")
-def train(session, load_epoch, lr, epochs):
+@click.option("--load-epoch", default=0, help="Completed epoch to start from")
+@click.option("--lr", default=5e-4, help="Learning rate")
+@click.option("--epochs", default=320, help="Epochs to train for")
+@click.option("--chips", default="chips", help="Chip source")
+@click.option("--test-chips", default="chips", help="Test chip source")  # fixme
+@click.option("--epoch-length", default=4 * 1024, help="Number of chips per epoch")
+@click.option("--test-length", default=64, help="Number of chips to test on")
+@click.option("--workers", default=0, help="Chip-loading processes")
+def train(
+    session,
+    load_epoch,
+    lr,
+    epochs,
+    chips,
+    test_chips,
+    epoch_length,
+    test_length,
+    workers,
+):
 
-    te = 0
+    physical_batch_size = 8 # parameterize
+    logical_batch_size = 32 # parameterize
+
+    loader_params = {
+        "batch_size": physical_batch_size,
+        "shuffle": True,
+        "num_workers": workers,
+        "pin_memory": True,
+    }
+
+    trainlen = epoch_length
+    testlen = test_length
+
+    Train = ChipReader(
+        chips,
+        trainlen,
+        0,
+    )
+
+    Test = ChipReader(test_chips, testlen, trainlen)
+
+    trainloader = DataLoader(Train, **loader_params)
+    testloader = DataLoader(Test, **loader_params)
+
+    epoch_counter = 0
 
     gen = Ripple().to(device)
 
@@ -162,11 +165,9 @@ def train(session, load_epoch, lr, epochs):
     for epoch in range(epochs):
         losses = []
 
-        ct = 0
-
-        with tqdm(trainloader, unit=" b", mininterval=2) as progress:
+        with tqdm(trainloader, unit="b", mininterval=2) as progress:
             for x, y in progress:
-                progress.set_description(f"Ep {te}")
+                progress.set_description(f"Ep {epoch_counter}")
 
                 x = x.to(device, non_blocking=True)
                 y = y.to(device, non_blocking=True)
@@ -174,7 +175,7 @@ def train(session, load_epoch, lr, epochs):
                 gen.train()
                 ŷ = gen(x)
 
-                ok_loss = ΔEuOK(y, ŷ) * 100
+                ok_loss = ΔEOK(y, ŷ, c_weight=2.5, h_weight=2.5) * 25
                 wave_loss = big_pyramid_loss(y, ŷ) * 250
                 loss = wave_loss + ok_loss
 
@@ -193,7 +194,7 @@ def train(session, load_epoch, lr, epochs):
 
                     batch_counter = 0
 
-            log.add_scalars("loss", {"train": np.mean(np.array(losses))}, te)
+            log.add_scalars("loss", {"train": np.mean(np.array(losses))}, epoch_counter)
             log.flush()
 
             if epoch % 1 == 0:
@@ -209,7 +210,7 @@ def train(session, load_epoch, lr, epochs):
                         )
 
                     for x, y in testloader:
-                        progress.set_description(f"Ep {te}")
+                        progress.set_description(f"Ep {epoch_counter}")
                         x = x.to(device, non_blocking=True)
                         y = y.to(device, non_blocking=True)
 
@@ -222,10 +223,10 @@ def train(session, load_epoch, lr, epochs):
 
                         testlosses.append(float(test_loss.item()))
 
-                log.add_scalars("loss", {"test": np.mean(np.array(testlosses))}, te)
+                log.add_scalars("loss", {"test": np.mean(np.array(testlosses))}, epoch_counter)
                 log.flush()
 
-            te += 1
+            epoch_counter += 1
 
 
 log = SummaryWriter()
