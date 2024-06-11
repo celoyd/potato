1aa0c5913901a620d8f49b32857002e62900e585
celoyd <celoyd@gmail.com>
2024-06-11 14:41:37 -0700

    Model update

--- ripple/model.py
+++ ripple/model.py
@@ -13,117 +13,112 @@ from ripple.color import BandsToOklab
 
 from torch.nn.utils.parametrizations import weight_norm
 
-# c = Mish()
-c = LeakyReLU(negative_slope=1/16)
-
-n = 48
-
 def concat(*a):
     return torch.cat([*a], dim=1)
 
 class ConvChunk(nn.Module):
-    def __init__(self, in_depth, out_depth, drop=False):
+    '''
+    A lightweight, nonspecialized image processing convolutional chunk.
+    Takes input and output depths so it can be used to stretch or squeeze.
+
+    The main branch of this block basically does this twice:
+
+    1×1 | dropout | 3×3 (4 groups) | leaky relu
+
+    and then ends with a final 1x1.
+
+    '''
+    def __init__(self, in_depth, out_depth, dropout=True):
         super().__init__()
 
-        mid_depth = int(
-            0.5
-            + torch.exp(
-                (torch.log(torch.tensor(in_depth)) + torch.log(torch.tensor(out_depth)))
-                / 2
-            )
+        # Use an int multiplicatively midway between the in and out depths
+        # as the depth of most operations inside the block
+        mid_depth = (in_depth * out_depth)**0.5
+
+        # round up to next multiple of 8
+        mid_depth = int((mid_depth + 8 - 1) // 8) * 8
+
+        self.nl = LeakyReLU(negative_slope=1/16)
+
+        if dropout:
+            self.drop = nn.Dropout(0.25)
+        else:
+            self.drop = lambda x: x
+
+        self.skip = nn.Conv2d(in_depth, out_depth, 1)
+        self.front_pointwise = nn.Conv2d(in_depth, mid_depth, 1)
+
+        self.front_conv = nn.Conv2d(
+            mid_depth, mid_depth, 3, groups=8, padding=1, padding_mode="reflect"
         )
 
-        self.should_drop = drop
-        if self.should_drop:
-            self.drop = nn.Dropout(0.5)
+        self.middle_pointwise = nn.Conv2d(mid_depth, mid_depth, 1)
 
-        mid_depth = (mid_depth // 4 + 1) * 4
-        self.mid_depth = mid_depth
-
-        self.nl = c
-
-        self.res = nn.Conv2d(in_depth, out_depth, 1)
-        self.res.mma_norm = True
-
-        self.front = nn.Conv2d(in_depth, mid_depth, 1)
-        self.front.mma_norm = True
-
-        self.A = nn.Conv2d(
-            mid_depth, mid_depth, 3, groups=4, padding=1, padding_mode="reflect"
+        self.back_conv = nn.Conv2d(
+            mid_depth, mid_depth, 3, groups=8, padding=1, padding_mode="reflect"
         )
-        self.A.mma_norm = True
 
-        self.B = nn.Conv2d(mid_depth, mid_depth, 1)
-        self.B.mma_norm = True
-
-        self.C = nn.Conv2d(
-            mid_depth, mid_depth, 3, groups=4, padding=1, padding_mode="reflect"
-        )
-        self.C.mma_norm = True
-
-        self.D = nn.Conv2d(mid_depth, out_depth, 1)
-        self.D.mma_norm = True
+        self.end_pointwise = nn.Conv2d(mid_depth, out_depth, 1)
 
     def forward(self, x):
 
-        res = self.res(x)
-        x = self.front(x)
+        skip = self.skip(x)
+
+        x = self.front_pointwise(x)
+        x = self.drop(x)
+        x = self.front_conv(x)
         x = self.nl(x)
 
-        x = self.A(x)
+        x = self.middle_pointwise(x)
+        x = self.drop(x)
+        x = self.back_conv(x)
         x = self.nl(x)
-        x = self.B(x)
 
-        if self.should_drop:
-            x = self.drop(x)
-
-        x = self.C(x)
-        x = self.D(x) + res
-
-        return x
+        x = self.end_pointwise(x)
+        return x + skip
 
 
-class Double(nn.Module):
-    def __init__(self, in_depth, out_depth):
-        super().__init__()
-        self.squeezing = in_depth != out_depth
+# class Double(nn.Module):
+#     def __init__(self, in_depth, out_depth):
+#         super().__init__()
+#         self.squeezing = in_depth != out_depth
 
-        if self.squeezing:
-            self.squeeze = nn.Conv2d(in_depth, out_depth, 1, padding=0)
-            self.squeeze.mma_norm = True
+#         if self.squeezing:
+#             self.squeeze = nn.Conv2d(in_depth, out_depth, 1, padding=0)
 
-        self.ups = nn.Upsample(scale_factor=2, mode="bilinear")
+#         self.ups = nn.Upsample(scale_factor=2, mode="bilinear")
 
-    def forward(self, x):
-        if self.squeezing:
-            x = self.squeeze(x)
-        return self.ups(x)
+#     def forward(self, x):
+#         if self.squeezing:
+#             x = self.squeeze(x)
+#         return self.ups(x)
 
 
 class Ripple(nn.Module):
-    def __init__(self):
+    def __init__(self, n=48):
         super().__init__()
 
-        self.bili_up = nn.Upsample(scale_factor=2, mode="bilinear")
+        self.zoom = nn.Upsample(scale_factor=2, mode="bilinear")
 
         self.oklab = BandsToOklab()
 
         self.bq = ConvChunk(3 + 8 + 16, n)
-        self.eq = ConvChunk(n, n // 2, drop=False)
-        self.q_to_h = Double(n // 2, n // 2)
+        self.eq = ConvChunk(n, n // 2)
 
         self.bh = ConvChunk(n // 2 + 4 + 3, n)
-        self.eh = ConvChunk(n, n // 2, drop=False)
-        self.h_to_f = Double(n // 2, n // 2)
+        self.eh = ConvChunk(n, n // 2)
 
-        self.bf = ConvChunk(n // 2 + 1 + 3 + 1, 3, drop=False)
+        # self.bf = ConvChunk(n // 2 + 1 + 3 + 1, n // 2)
+        self.f = ConvChunk(n // 2 + 1 + 3 + 1, 3, dropout=False)
+        # self.ef = ConvChunk(n // 2, 3, dropout=False)
 
     def forward(self, x):
         x = torch.clamp(x, 1e-9, None)
         oklab = self.oklab(x[:, 16:])
-        oklab_half = self.bili_up(oklab)
-        oklab_full = self.bili_up(oklab_half)
-        # return 0, 0, 0, oklab_full
+        oklab_half = self.zoom(oklab)
+        oklab_full = self.zoom(oklab_half)
+
+        # return 0, 0, oklab_full
 
         x = torch.pow(x, 1 / 3)
 
@@ -131,9 +126,12 @@ class Ripple(nn.Module):
         pan_full = tile(pan_quarter, 4)
         pan_half = pile(pan_full, 2)
 
-        pan_detail = pan_full - self.bili_up(
-            self.bili_up(pan_quarter.mean(axis=-3, keepdim=True))
+        pan_detail = pan_full - self.zoom(
+            self.zoom(pan_quarter.mean(axis=-3, keepdim=True))
         )
+
+        cheap_sharp = oklab_full.clone()
+        cheap_sharp[:, 0:1] += pan_detail
 
         mul = x[:, 16:]
 
@@ -142,19 +140,17 @@ class Ripple(nn.Module):
         ql = q[:, :3]
         q = self.eq(q)
 
-        h = self.q_to_h(q)
+        h = self.zoom(q)
 
-        h = concat(oklab_half, pan_half, h)
+        h = concat(h, pan_half, oklab_half)
         h = self.bh(h)
         hl = h[:, :3]
         h = self.eh(h)
 
-        f = self.h_to_f(h)
+        f = self.zoom(h)
 
         f = concat(f, pan_full, oklab_full, pan_detail)
-        f = self.bf(f)
 
-        # oklab_full[:, 0:1] += pan_detail
-        # return 0, 0, 0, oklab_full
+        f = self.f(f)
 
-        return ql, hl, f, f
+        return ql, hl, cheap_sharp + f
