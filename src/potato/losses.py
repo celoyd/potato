e44abfa2832a2bbd765c1d6f2c437709a0a4e5f7
celoyd <celoyd@gmail.com>
2025-02-20 23:08:55 -0800

    Better rfft loss(es)

--- src/potato/losses.py
+++ src/potato/losses.py
@@ -1,9 +1,14 @@
 import torch
+from potato.util import cheap_half
 
+def remap(t, a, b, x, y):
+    return x + (t - a) * (y - x) / (b - a)
 
 def oklab_saturation(x):
     return torch.sqrt(torch.square(x[:, 1]) + torch.square(x[:, 2]))
 
+def sat_loss(y, ŷ):
+    return (oklab_saturation(y) - oklab_saturation(ŷ)).abs().mean()
 
 def ΔEOK(y, ŷ, ab_weight=2.0):
     """
@@ -24,37 +29,28 @@ def ΔEOK(y, ŷ, ab_weight=2.0):
         * (torch.square(y[:, 1] - ŷ[:, 1]) + torch.square(y[:, 2] - ŷ[:, 2]))
     ).sqrt().mean() * 50
 
-
-def rfft_loss(y, ŷ):
-    fy = torch.fft.rfft(y)
-    fŷ = torch.fft.rfft(ŷ)
-    return torch.mean(torch.abs(fy - fŷ))
-
-
-def rfft_texture_loss(y, ŷ):
+def detail_loss(y, ŷ):
     dev = y.device
-    yfft = torch.abs(torch.fft.rfft2(y))
-    ŷfft = torch.abs(torch.fft.rfft2(ŷ))
 
-    h, w = yfft.shape[-2:]
+    fty = torch.abs(torch.fft.rfft2(y))
+    ftŷ = torch.abs(torch.fft.rfft2(ŷ))
 
-    vertical = torch.arange(h, device=dev) / h
-    horizontal = torch.arange(w, device=dev) / (w * 2)
+    h, w = fty.shape[-2:]
 
-    coords = torch.cartesian_prod(vertical, horizontal)
-    pt = torch.tensor([0.5, 0], device=dev).unsqueeze(0)
-    distances = torch.cdist(pt, coords)
-    distances = distances.reshape(h, w) * 2**0.5
+    fty = fty.roll(h//2, 0)
+    ftŷ = ftŷ.roll(h//2, 0)
 
-    mask = distances**0.5
+    u, v = torch.meshgrid(
+        torch.arange(h, device=dev) / h,
+        (torch.arange(w, device=dev) / w - 0.5) * 2,
+        indexing="ij",
+    )
 
-    diff = torch.mean(torch.abs(yfft - ŷfft) * mask)
+    dist = torch.sqrt(torch.square(u) + torch.square(v))
+    mask = remap(dist, 6/16, 7/16, 0, 1).clip(0, 1)
 
-    return diff
+    diff = (mask*fty - mask*ftŷ)
+    return diff.abs().mean()
 
-
-def rfft_saturation_loss(y, ŷ):
-    y_sat = oklab_saturation(y)
-    ŷ_sat = oklab_saturation(ŷ)
-
-    return rfft_texture_loss(y_sat, ŷ_sat)
+def sat_detail_loss(y, ŷ):
+    return detail_loss(oklab_saturation(y), oklab_saturation(ŷ))
